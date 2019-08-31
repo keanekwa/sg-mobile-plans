15dbaeada984f4325afb8540d6be208553731543
keanekwa <hello@keanekwa.com>
2019-08-31 13:55:02 +0800

    Removed React Input Range fully (not clean removal on previous commit)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'react-input-range/lib/css/index.css';
 import App from './App/App.js'
 
 ReactDOM.render(<App />, document.getElementById('root'));
