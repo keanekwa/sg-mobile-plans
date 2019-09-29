b761b1d9733d1ecbd1e12599bdfbb238056b727e
keanekwa <hello@keanekwa.com>
2019-09-29 10:42:34 +0800

    Changed Result to use ExpansionPanel

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import Collapse from '@material-ui/core/Collapse';
-import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import { blueGrey } from '@material-ui/core/colors/';
 
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 const useStyles = makeStyles({
-  root: {
-    margin: '1.25rem 0 0 0',
-    textAlign: 'left',
-    backgroundColor: blueGrey[800],
-  },
-  button: {
-    justifyContent: 'left',
-    backgroundColor: blueGrey[900],
-  },
-  data: {
-    padding: '0 1rem 1rem 1rem',
+  ExpansionPanel: {
     background: 'none',
     color: blueGrey[50],
+    border: '1px solid ' + blueGrey[50],
   },
+  ExpandMoreIcon: {
+    color: blueGrey[50],
+  },
+  ExpansionPanelSummary: {
+    padding: '0 1.5rem',
+  },
+  ExpansionPanelDetails: {
+    backgroundColor: blueGrey[800],
+    padding: '1.5rem',
+    flexDirection: 'column',
+  },
+  price: {
+    color: blueGrey[200],
+  }
 });
 
 export default function Result(props) {
@@ -39,25 +47,24 @@ export default function Result(props) {
     cons = props.cons.map(con => <li key={con}>{con}</li>);
   }
 
-  const [checked, setChecked] = React.useState(false);
-  const expandOrMinimize = () => {
-    setChecked(prev => !prev);
-  }
-
   return (
-    <Collapse className={classes.root} in={checked} collapsedHeight='56px'>
-      <Button size='large' className={classes.button} component='div' color='primary' variant='outlined' fullWidth={true} onClick={expandOrMinimize}>
-        {props.telco} {props.planName} --- ${props.price % 1 === 0 ? props.price : props.price.toFixed(2)}
-      </Button>
-      <Box className={classes.data} square={true}>
-        Data: {props.data}{props.data === 'Unlimited' ? '' : 'GB'}<br/>
-        Talktime: {props.talktime}{props.talktime === 'Unlimited' ? '' : 'min'}<br/>
-        SMS: {props.sms}<br/>
-        {pros === '' ? '' : 'Pros:'}
-        {pros === '' ? '' : <ul>{pros}</ul>}
-        {cons === '' ? '' : 'Cons:'}
-        {cons === '' ? '' : <ul>{cons}</ul>}
-      </Box>
-    </Collapse>
+    <ExpansionPanel square={true} className={classes.ExpansionPanel}>
+      <ExpansionPanelSummary className={classes.ExpansionPanelSummary} disableRipple={false} disableTouchRipple={false} expandIcon={<ExpandMoreIcon className={classes.ExpandMoreIcon}/>}>
+        {props.telco} {props.planName} <Box className={classes.price}>&nbsp;| ${props.price % 1 === 0 ? props.price : props.price.toFixed(2)}</Box>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails className={classes.ExpansionPanelDetails}>
+        <Box>
+          Data: {props.data}{props.data === 'Unlimited' ? '' : 'GB'}<br/>
+          Talktime: {props.talktime}{props.talktime === 'Unlimited' ? '' : 'min'}<br/>
+          SMS: {props.sms}<br/>
+        </Box>
+        <Box>
+          {pros === '' ? '' : 'Pros:'}<br/>
+          {pros === '' ? '' : <ul>{pros}</ul>}
+          {cons === '' ? '' : 'Cons:'}<br/>
+          {cons === '' ? '' : <ul>{cons}</ul>}
+        </Box>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   );
 }
